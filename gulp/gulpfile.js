6717b23d83b0e6c0e2c5b2c39ea542d1dd9d30f7
pak <dummy>
2015-04-25 17:06:13 +0900

    開発ベース環境構築

---
+++ gulp/gulpfile.js
@@ -0,0 +1,74 @@
+var gulp = require('gulp');
+var fs = require('fs');
+
+var concat = require('gulp-concat');
+var compass = require('gulp-compass');
+var typescript = require('gulp-typescript');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var del = require('del');
+var plumber = require('gulp-plumber');
+
+
+var BUILD_DIR = '../build/';
+var SRC_DIR = '../src/main/';
+
+
+gulp.task('initdir', function(){
+	if(!fs.existsSync(BUILD_DIR + 'js')){
+		fs.mkdir(BUILD_DIR + 'js');
+	}
+	if(!fs.existsSync(BUILD_DIR + 'css')){
+		fs.mkdir(BUILD_DIR + 'css');
+	}
+	if(!fs.existsSync(BUILD_DIR + 'lib')){
+		fs.mkdir(BUILD_DIR + 'lib');
+	}
+	if(!fs.existsSync(BUILD_DIR + 'images')){
+		fs.mkdir(BUILD_DIR + 'images');
+	}
+	if(!fs.existsSync(BUILD_DIR + 'site')){
+		fs.mkdir(BUILD_DIR + 'site');
+	}
+});
+
+gulp.task('clean', function(cb){
+	del([BUILD_DIR + 'js/*.js'], {force : true});
+	del([BUILD_DIR + 'css/*.css'], {force : true});
+	del([BUILD_DIR + 'site/**/*'], {force : true});
+	del([BUILD_DIR + 'lib/**/*'], {force : true});
+});
+
+gulp.task('copy-php', function(){
+	return gulp.src([SRC_DIR + 'index.php', SRC_DIR + '.htaccess', SRC_DIR + 'site/**/*'], {base : SRC_DIR})
+		.pipe(gulp.dest(BUILD_DIR));
+});
+gulp.task('copy-lib',function(){
+	return gulp.src([SRC_DIR + 'lib/**/*'], {base : SRC_DIR})
+		.pipe(gulp.dest(BUILD_DIR));
+})
+
+gulp.task('typescript-compile', function(){
+	return gulp.src(['../src/main/ts/**/*'])
+		.pipe(typescript({ target : 'ES5', removeComments: true, noExternalResolve: true}))
+		.js
+		.pipe(uglify())
+		.pipe(gulp.dest(BUILD_DIR + 'js/'));
+});
+gulp.task('compass-compile', function(){
+	return gulp.src([SRC_DIR + 'sass/**/*'])
+		.pipe(plumber({errorHandler: function(e){console.log(e.toString());}}))
+		.pipe(compass({
+			comments: false,
+			css: BUILD_DIR +'css/',
+			sass: SRC_DIR +'scss/',
+			image: BUILD_DIR +'images/'
+		}))
+		.pipe(minifyCss())
+		.pipe(gulp.dest(BUILD_DIR + 'css/'));
+});
+
+
+gulp.task('compile-all', ['initdir', 'clean', 'copy-lib', 'copy-php', 'typescript-compile', 'compass-compile'], function(){
+
+});
---
+++ readme.txt
@@ -0,0 +1,69 @@
+###########################################################
+# ヒューライツ大阪　名簿システム ver2
+#
+###########################################################
+
+■ 1. プロジェクトについて
+
+	ヒューライツ大阪　名簿管理システムの新規システムです。
+
+
+■ 2. 言語 / ライブラリ等
+
+□PHP
+	・PHP 5.1以上
+	・MVCフレームワーク：Curry PHP
+	・テンプレート　　 ：Smarty
+
+□JavaScript
+	・AltJSとしてTypeScriptを採用
+	・jQueryベース
+
+□CSS
+	・Sass / Compassを採用
+
+□ビルドシステム
+	・gulpを採用
+
+□MySQL 5.1
+
+
+■ 3. 開発環境について
+
+□Eclipse 4.4
+
+□ER Master
+
+□Node.js
+
+・以下インストール必須です。
+
+	・Node.js　http://nodejs.jp/
+
+	・Gulp
+	・Gulp-typescript
+	・TypeScript
+
+Node.jsのパッケージインストールコマンドまとめ
+-----------------------------------------
+npm install -g gulp
+npm install -g typescript
+npm install -g gulp-typescript
+-----------------------------------------
+
+□Ruby
+
+・以下インストール必須です。
+
+	・Ruby https://www.ruby-lang.org/ja/
+
+	・Sass
+	・Compass
+
+
+Rubyのパッケージインストールコマンドまとめ
+-----------------------------------------
+gem update --system
+gem install sass
+gem install compass
+-----------------------------------------
