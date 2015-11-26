e3279f7fd2b1e1a10cdc0b08ed9a15d9a2b7a30e
pak <dummy>
2015-11-26 22:50:42 +0900

    gulp - add no-clean tasks

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -19,132 +19,160 @@ var BUILD_DIR = path.resolve('../build') + '/';
 var SRC_DIR = path.resolve('../src/main') + '/';
 
 
+var Exec = {};
+
+(function(Exec){
+    Exec.init_dir = function(){
+        if(!fs.existsSync(BUILD_DIR + 'js')){
+            fs.mkdir(BUILD_DIR + 'js');
+        }
+        if(!fs.existsSync(BUILD_DIR + 'css')){
+            fs.mkdir(BUILD_DIR + 'css');
+        }
+        if(!fs.existsSync(BUILD_DIR + 'html')){
+            fs.mkdir(BUILD_DIR + 'html');
+        }
+        if(!fs.existsSync(BUILD_DIR + 'lib')){
+            fs.mkdir(BUILD_DIR + 'lib');
+        }
+        if(!fs.existsSync(BUILD_DIR + 'images')){
+            fs.mkdir(BUILD_DIR + 'images');
+        }
+        if(!fs.existsSync(BUILD_DIR + 'site')){
+            fs.mkdir(BUILD_DIR + 'site');
+        }
+        if(!fs.existsSync(BUILD_DIR + 'site/logs')){
+            fs.mkdir(BUILD_DIR + 'site/logs');
+        }
+    };
+
+    Exec.clean_php = function(cb) {
+        del(
+            [
+                BUILD_DIR + 'site/app/**/*',
+                BUILD_DIR + 'site/configs/**/',
+                BUILD_DIR + 'site/data/**/*',
+                BUILD_DIR + 'site/library/**/*',
+            ], {force : true},
+            cb);
+    };
+
+    Exec.clean_lib = function(cb) {
+        del(
+            [
+                BUILD_DIR + 'lib/**/*'
+            ], {force : true},
+            cb);
+    };
+
+    Exec.clean_webcontent = function(cb){
+        del(
+            [
+                SRC_DIR + 'css/**/*',
+                SRC_DIR + 'js/**/*',
+                BUILD_DIR + 'js/*.js',
+                BUILD_DIR + 'css/*.css',
+                BUILD_DIR + 'html/**/*',
+            ], {force : true},
+            cb);
+    };
+
+    Exec.copy_php = function() {
+        gulp.src(
+            [
+                SRC_DIR + 'index.php',
+                SRC_DIR + '.htaccess',
+                SRC_DIR + 'curry/**/*',
+                SRC_DIR + 'curry/.htaccess', // 何故無視されるんだ。。
+                SRC_DIR + 'site/app/**/*',
+                SRC_DIR + 'site/.htaccess',
+                SRC_DIR + 'site/configs/**/*',
+                SRC_DIR + 'site/data/**/*',
+                SRC_DIR + 'site/library/**/*',
+            ],
+            {base: SRC_DIR})
+            .pipe(gulp.dest(BUILD_DIR));
+    };
+
+    Exec.copy_html = function(){
+        gulp.src(
+            [
+                SRC_DIR + 'html/**/*',
+            ],
+            {base : SRC_DIR})
+            .pipe(gulp.dest(BUILD_DIR))
+    };
+
+    Exec.copy_lib = function(){
+        gulp.src([SRC_DIR + 'lib/**/*'], {base : SRC_DIR})
+            .pipe(gulp.dest(BUILD_DIR));
+    };
+
+    Exec.ts_compile = function(){
+        return gulp.src([SRC_DIR + 'ts/**/*.ts'])
+            .pipe(typescript({ target : 'ES5', removeComments: false, noExternalResolve: true, module: 'amd'}))
+            .js
+            .pipe(gulp.dest(SRC_DIR + 'js/'))
+            .on('end', function(){
+                // main/js/impl以下のファイルだけ圧縮してbuildへコピー
+                gulp.src([SRC_DIR + 'js/impl/**/*.js'])
+                    .pipe(uglify())
+                    .pipe(gulp.dest(BUILD_DIR + 'js/'));
+            });
+    };
+
+    Exec.compass_compile = function(){
+        return gulp.src([SRC_DIR + 'sass/*.scss'])
+            .pipe(plumber({errorHandler: notify.onError('<%= error.message %>')}))
+            .pipe(compass({
+                comments: false,
+                project: SRC_DIR,
+                css: 'css',
+                scss: SRC_DIR + 'sass',
+                image: SRC_DIR + 'sass/images'
+            }))
+            .pipe(minifyCss())
+            .pipe(gulp.dest(BUILD_DIR + 'css/'));
+    };
+})(Exec);
 
 //
 // タスク定義
 //
 
 // ビルドディレクトリ初期化
-gulp.task('initdir', function(){
-	if(!fs.existsSync(BUILD_DIR + 'js')){
-		fs.mkdir(BUILD_DIR + 'js');
-	}
-	if(!fs.existsSync(BUILD_DIR + 'css')){
-		fs.mkdir(BUILD_DIR + 'css');
-	}
-	if(!fs.existsSync(BUILD_DIR + 'html')){
-		fs.mkdir(BUILD_DIR + 'html');
-	}
-	if(!fs.existsSync(BUILD_DIR + 'lib')){
-		fs.mkdir(BUILD_DIR + 'lib');
-	}
-	if(!fs.existsSync(BUILD_DIR + 'images')){
-		fs.mkdir(BUILD_DIR + 'images');
-	}
-	if(!fs.existsSync(BUILD_DIR + 'site')){
-		fs.mkdir(BUILD_DIR + 'site');
-	}
-	if(!fs.existsSync(BUILD_DIR + 'site/logs')){
-		fs.mkdir(BUILD_DIR + 'site/logs');
-	}
-});
+gulp.task('initdir', Exec.init_dir);
 
-gulp.task('clean-php', ['initdir'], function(cb){
-	del(
-		[
-			BUILD_DIR + 'site/app/**/*',
-			BUILD_DIR + 'site/configs/**/',
-			BUILD_DIR + 'site/data/**/*',
-			BUILD_DIR + 'site/library/**/*',
-		], {force : true},
-		cb);
-});
-gulp.task('clean-lib', ['initdir'], function(cb){
-	del(
-		[
-			BUILD_DIR + 'lib/**/*'
-		], {force : true},
-		cb);
-});
-gulp.task('clean-webcontent', ['initdir'], function(cb){
-	del(
-		[
-			SRC_DIR + 'css/**/*',
-			SRC_DIR + 'js/**/*',
-			BUILD_DIR + 'js/*.js',
-			BUILD_DIR + 'css/*.css',
-			BUILD_DIR + 'html/**/*',
-		], {force : true},
-		cb);
-});
+gulp.task('clean-php', ['initdir'], Exec.clean_php);
+gulp.task('clean-lib', ['initdir'], Exec.clean_lib);
+gulp.task('clean-webcontent', ['initdir'], Exec.clean_webcontent);
 
 // 前回ビルド結果削除
 gulp.task('clean', ['clean-php', 'clean-lib', 'clean-webcontent']);
 
 // PHPファイルコピー
-gulp.task('copy-php',['clean-php'], function(){
-		gulp.src(
-				[
-					SRC_DIR + 'index.php',
-					SRC_DIR + '.htaccess',
-					SRC_DIR + 'curry/**/*',
-					SRC_DIR + 'curry/.htaccess', // 何故無視されるんだ。。
-					SRC_DIR + 'site/app/**/*',
-					SRC_DIR + 'site/.htaccess',
-					SRC_DIR + 'site/configs/**/*',
-					SRC_DIR + 'site/data/**/*',
-					SRC_DIR + 'site/library/**/*',
-				],
-				 {base : SRC_DIR})
-			.pipe(gulp.dest(BUILD_DIR))
-});
+gulp.task('copy-php',['clean-php'], Exec.copy_php);
+gulp.task('copy-php-noclean', Exec.copy_php);
 
-gulp.task('copy-html', function(){
-	gulp.src(
-		[
-			SRC_DIR + 'html/**/*',
-		],
-		{base : SRC_DIR})
-		.pipe(gulp.dest(BUILD_DIR))
-})
+// HTMLファイルコピー
+gulp.task('copy-html', Exec.copy_html);
 
 // ライブラリコピー
-gulp.task('copy-lib',['clean-lib'], function(){
-	gulp.src([SRC_DIR + 'lib/**/*'], {base : SRC_DIR})
-		.pipe(gulp.dest(BUILD_DIR));
-});
+gulp.task('copy-lib',['clean-lib'], Exec.copy_lib);
+gulp.task('copy-lib-noclean', Exec.copy_lib);
 
 // Typescriptコンパイル
-gulp.task('typescript-compile', function(){
-	return gulp.src([SRC_DIR + 'ts/**/*.ts'])
-		.pipe(typescript({ target : 'ES5', removeComments: false, noExternalResolve: true, module: 'amd'}))
-		.js
-		.pipe(gulp.dest(SRC_DIR + 'js/'))
-		.on('end', function(){
-			// main/js/impl以下のファイルだけ圧縮してbuildへコピー
-			gulp.src([SRC_DIR + 'js/impl/**/*.js'])
-				.pipe(uglify())
-				.pipe(gulp.dest(BUILD_DIR + 'js/'));
-		});
-});
+gulp.task('typescript-compile', Exec.ts_compile);
 
 // Compassコピー
-gulp.task('compass-compile', function(){
-	return gulp.src([SRC_DIR + 'sass/*.scss'])
-		.pipe(plumber({errorHandler: notify.onError('<%= error.message %>')}))
-		.pipe(compass({
-			comments: false,
-			project: SRC_DIR,
-			css: 'css',
-			scss: SRC_DIR + 'sass',
-			image: SRC_DIR + 'sass/images'
-		}))
-		.pipe(minifyCss())
-		.pipe(gulp.dest(BUILD_DIR + 'css/'));
-});
+gulp.task('compass-compile', Exec.compass_compile);
 
-// 全コンパイル
-gulp.task('compile-all', ['initdir', 'clean', 'copy-lib', 'copy-php', 'copy-html', 'typescript-compile', 'compass-compile']);
+// clean-全コンパイル
+gulp.task('rebuild', ['initdir', 'clean', 'copy-lib', 'copy-php', 'copy-html', 'typescript-compile', 'compass-compile']);
+
+// cleanせずbuild
+gulp.task('build', ['initdir', 'copy-lib-noclean', 'copy-php-noclean', 'copy-html', 'typescript-compile', 'compass-compile']);
+
 
 // コマンドラインで常駐させて、変更があったscssのみ再コンパイル
 gulp.task('watch-compass', function(){
