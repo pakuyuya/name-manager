13c8e0c2f4317db63d4fa89a853162fdf75218fb
pakuyuya <pakuyuya@yahoo.co.jp>
2017-03-20 14:31:48 +0900

    Toastテストコード削除

--- src/main/ts/app/app/main.ts
+++ src/main/ts/app/app/main.ts
@@ -18,13 +18,6 @@ import {NamesListDirectiveController} from '../directives/namesList';
 import {AddNameDialogDirectiveController} from '../directives/addNameDialog';
 import {MemberTypeStoreService} from '../services/memberTypeStoreService';
 
-import {Toast} from '../common/toast';
-
-Toast.push('a');
-Toast.push('a');
-Toast.push('a');
-
-
 app.controller('HeaderCtrl', ['$scope', function($scope){
 }]);
 
